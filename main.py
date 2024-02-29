d7cc0fb73b439e23f3766cb04c9ae35335e65115
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-02-29 14:10:32 +0100

    input tensor

--- main.py
+++ main.py
@@ -13,16 +13,27 @@ dtype = torch.float
 device = torch.device("mps")  # mps for Apple Metal, torch.device("cuda") for Nvidia GPU's or otherwise torch.device("cpu") to use CPU
 
 
-def plot_ecg():
-    record = wfdb.rdrecord(f'{physio_root}/01/010/JS00001') 
-    wfdb.plot_wfdb(record=record, title='01 010')
+def plot_ecg(i):
+    record = wfdb.rdrecord(f'{physio_root}/01/010/JS00{i:03}') 
+    wfdb.plot_wfdb(record=record, title=f'01 010 i')
     
 
-def read_ecg():
-    signals, fields = wfdb.rdsamp(f'{physio_root}/01/010/JS00001')
-    input_tensor = torch.Tensor(signals)
+def create_input_tensor():
+    print(f'creating input tensor')
+    init_sample, init_field = wfdb.rdsamp(f'{physio_root}/01/010/JS00001')
+    input_tensor = torch.Tensor(np.array(init_sample)).unsqueeze(0)
+
+    for i in range(2, 104):
+        try: 
+            sample_values, sample_field = wfdb.rdsamp(f'{physio_root}/01/010/JS00{i:03}')
+            sample_tensor = torch.Tensor(np.array(sample_values)).unsqueeze(0)
+            input_tensor = torch.cat([input_tensor, sample_tensor], dim=0)
+
+        except FileNotFoundError:
+            print(f"FileNotFoundError for sample {i}, skipping...")
+            continue
+
     print(input_tensor.size())
-    print(signals)
 
     
 
@@ -37,5 +48,5 @@ if __name__ == "__main__":
     # setup_seed(args.seed)
     
     # plot_ecg()
-    read_ecg()
+    create_input_tensor()
 
