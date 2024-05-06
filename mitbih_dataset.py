d9b0ed968e7081e8872105726f7daededa3e4ae6
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-05-06 17:50:15 +0200

    testing on server

---
+++ mitbih_dataset.py
@@ -0,0 +1,92 @@
+import json
+import matplotlib.pyplot as plt
+import tqdm
+import random
+import cv2
+from os.path import isfile, join
+from os import listdir
+import os
+import wfdb
+import numpy as np
+
+_range_to_ignore = 20
+_directory = '../data/mitbih/'
+_dataset_dir = '../data/dataset_filtered/'
+_dataset_ann_dir = '../data/dataset_ann/'
+_split_percentage = .70
+_split_validation_percentage = 0.70
+_split_test_percentage = 0.50
+
+def create_img_from_sign(lblabels, lbrevert_labels, lboriginal_labels, size=(128, 128), augmentation=True):
+    """
+       For each beat for each patient creates img apply some filters
+       :param size: the img size
+       :param augmentation: create for each image another nine for each side
+    """
+
+    if not os.path.exists(_directory):
+        os.makedirs(_directory)
+
+    files = [f[:-4] for f in listdir(_directory) if isfile(join(_directory, f)) if (f.find('.dat') != -1)]
+
+    random.shuffle(files)
+    train = files[: int(len(files) * _split_percentage)]
+    test = files[int(len(files) * _split_percentage):]
+
+    for file in files:
+        sig, _ = wfdb.rdsamp(_directory + file)
+        ann = wfdb.rdann(_directory + file, extension='atr')
+        for i in tqdm.tqdm(range(1, len(ann.sample) - 1)):
+            print(ann)
+            if ann.symbol[i] not in lboriginal_labels:
+                continue
+            label = lboriginal_labels[ann.symbol[i]]
+            if file in train:
+                dir = '{}train/{}'.format(_dataset_dir, label)
+            else:
+                dir = '{}validation/{}'.format(_dataset_dir, label)
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+
+            ''' Get the Q-peak intervall '''
+            start = ann.sample[i - 1] + _range_to_ignore
+            end = ann.sample[i + 1] - _range_to_ignore
+
+            ''' Get the signals '''
+            plot_x = [sig[i][0] for i in range(start, end)]
+            plot_y = [i * 1 for i in range(start, end)]
+
+            ''' Plot and save the beat'''
+            fig = plt.figure(frameon=False)
+            plt.plot(plot_y, plot_x)
+            plt.xticks([]), plt.yticks([])
+            for spine in plt.gca().spines.values():
+                spine.set_visible(False)
+
+            ''' Convert in gray scale and resize img '''
+            if file in train:
+                filename = '{}train/{}/{}_{}{}{}0.png'.format(_dataset_dir, label, label, file[-3:], start, end)
+            else:
+                filename = '{}validation/{}/{}_{}{}{}0.png'.format(_dataset_dir, label, label, file[-3:], start, end)
+            fig.savefig(filename)
+            im_gray = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+            im_gray = cv2.resize(im_gray, size, interpolation=cv2.INTER_LANCZOS4)
+            cv2.imwrite(filename, im_gray)
+            # if augmentation:
+            #     cropping(im_gray, filename, size)
+            plt.cla()
+            plt.clf()
+            plt.close('all')
+
+
+
+
+if __name__ == "__main__":
+    labels_json = '{ ".": "N", "N": "N", "V": "V", "/": "Q", "L": "N", "R": "N", "A": "S", "a": "S", "J": "S", "S":"S", "F":"F", "e":"N", "j":"N", "E":"V", "f":"Q", "Q":"Q"}'
+    labels_to_float = '{ "N": "0", "S" : "1", "V": "2", "F": "3", "Q": "4"}'
+    float_to_labels = '{ "0": "N", "1" : "S", "2": "V", "3": "F", "4": "Q"}'
+    labels = json.loads(labels_to_float)
+    revert_labels = json.loads(float_to_labels)
+    original_labels = json.loads(labels_json)
+
+    create_img_from_sign(lblabels=labels, lbrevert_labels=revert_labels, lboriginal_labels=original_labels)
