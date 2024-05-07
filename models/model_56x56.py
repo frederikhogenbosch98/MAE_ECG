1fb11b7cbd7608a2c570c1073d51d19495e91dd9
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-05-07 11:55:25 +0200

    almsot back to convenext

--- models/model_56x56.py
+++ models/model_56x56.py
@@ -3,44 +3,26 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class EncoderBlock(nn.Module):
+class ResBlock(nn.Module):
 
-    def __init__(self, in_channels, out_channels, drop_path=0., layer_scale_init_value=1e-6):
+    def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
-        self.norm1 = nn.BatchNorm2d(in_channels)
-        self.norm2 = nn.BatchNorm2d(out_channels)
+        self.conv1 = nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=1)
+        self.norm1 = nn.BatchNorm2d(dim)
+        self.norm2 = nn.BatchNorm2d(dim)
         self.act = nn.GELU()
 
 
     def forward(self, x):
+        input = x
         x = self.conv1(x)
         x = self.norm1(x)
         x = self.act(x)
         x = self.conv2(x)
         x = self.norm2(x)
         x = self.act(x)
-        return x
-
-
-class DecoderBlock(nn.Module):
-
-    def __init__(self, in_channels, out_channels, drop_path=0., layer_scale_init_value=1e-6):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
-        self.norm = nn.BatchNorm2d(out_channels)
-        self.act = nn.GELU()
-
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.norm(x)
-        x = self.act(x)
-        x = self.conv2(x)
-        x = self.norm(x)
-        x = self.act(x)
+        x = input + x
         return x
 
 
@@ -50,34 +32,43 @@ class AutoEncoder56(nn.Module):
     def __init__(self, in_channels=1, channels=[32, 64, 128, 256], depths=[1, 1, 1, 1]):
         super(AutoEncoder56, self).__init__()
         self.encoder = nn.Sequential(
-            *[EncoderBlock(in_channels, channels[0]) for i in range(depths[0])],
+            nn.Conv2d(in_channels, channels[0], 3, stride=1, padding=1),
+            *[ResBlock(channels[0]) for i in range(depths[0])],
             # ResBlock(dim=channels[0]),
             nn.MaxPool2d(2, stride=2), 
-            *[EncoderBlock(channels[0], channels[1]) for i in range(depths[1])],
+            nn.Conv2d(channels[0], channels[1], 3, stride=1, padding=1),
+            *[ResBlock(channels[0], channels[1]) for i in range(depths[1])],
             # ResBlock(dim=channels[1]),
             nn.MaxPool2d(2, stride=2),  
-            *[EncoderBlock(channels[1], channels[2]) for i in range(depths[2])],
+            nn.Conv2d(channels[1], channels[2], 3, stride=1, padding=1),
+            *[ResBlock(channels[2]) for i in range(depths[2])],
             # ResBlock(dim=channels[2]),
             nn.MaxPool2d(2, stride=2),
-            *[EncoderBlock(channels[2], channels[3]) for i in range(depths[3])],
+            nn.Conv2d(channels[2], channels[3], 3, stride=1, padding=1),
+            *[ResBlock(channels[3]) for i in range(depths[3])],
             # ResBlock(dim=channels[3]),
             nn.MaxPool2d(2, stride=2),
             nn.Conv2d(channels[3], channels[3], kernel_size=7, stride=1, padding=0),
             nn.BatchNorm2d(channels[3]),
             nn.GELU(),
         )
+
         self.decoder = nn.Sequential(
             nn.Upsample(scale_factor=14, mode='bilinear'),
-            *[DecoderBlock(in_channels=channels[3], out_channels=channels[2]) for i in range(depths[3])],
+            nn.Conv2d(channels[3], channels[2], 3, stride=1, padding=1),
+            *[ResBlock(in_channels=channels[3], out_channels=channels[2]) for i in range(depths[3])],
             nn.Upsample(scale_factor=2, mode='bilinear'),
-            *[DecoderBlock(in_channels=channels[2], out_channels=channels[1]) for i in range(depths[2])],
+            nn.Conv2d(channels[2], channels[1], 3, stride=1, padding=1),
+            *[ResBlock(in_channels=channels[2], out_channels=channels[1]) for i in range(depths[2])],
             nn.Upsample(scale_factor=2, mode='bilinear'),
-            *[DecoderBlock(in_channels=channels[1], out_channels=channels[0]) for i in range(depths[1])],
+            nn.Conv2d(channels[1], channels[0], 3, stride=1, padding=1),
+            *[ResBlock(in_channels=channels[1], out_channels=channels[0]) for i in range(depths[1])],
             nn.Upsample(scale_factor=2, mode='bilinear'),
             nn.Conv2d(channels[0], channels[0], kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(channels[0]),
             nn.GELU(),
             nn.Conv2d(channels[0], in_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(in_channels),
             nn.GELU()
             )
 
@@ -94,12 +85,7 @@ class Classifier56(nn.Module):
         self.encoder = autoencoder.encoder
 
         self.norm = nn.LayerNorm(in_features, eps=1e-6) 
-        # self.classifier = nn.Sequential(
-        #     nn.Flatten(),
-        #     nn.Linear(in_features, in_features),
-        #     nn.GELU(),
-        #     nn.Linear(in_features, out_features)
-        # )
+
         # self.classifier = nn.Sequential(
         #         nn.Flatten(),
         #         nn.Linear(12544, 2048),
@@ -120,7 +106,6 @@ class Classifier56(nn.Module):
     def forward(self, x):
         x = self.encoder(x)
         # print(x.shape)
-        # x = self.norm(x.mean([-2,-1]))
         x = self.classifier(x)
         return x
 
