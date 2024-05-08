fa0067c0388d1a0ccd5278927952eaecab0719cb
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-05-08 10:23:15 +0200

    skip plotting

--- main.py
+++ main.py
@@ -215,29 +215,29 @@ def eval_mae(model, testset, batch_size=128):
 
     print(f'Average MSE Loss on Test Set: {average_loss}')
 
-    data_list = []
-    target_list = []
+    # data_list = []
+    # target_list = []
 
-    for data, _ in testset:
-        data_list.append(data.unsqueeze(0))
+    # for data, _ in testset:
+    #     data_list.append(data.unsqueeze(0))
 
-    test_data_tensor = torch.cat(data_list, dim=0)
+    # test_data_tensor = torch.cat(data_list, dim=0)
 
-    test_data_tensor = test_data_tensor.to(device)
+    # test_data_tensor = test_data_tensor.to(device)
 
 
-    x = model(test_data_tensor[0:64,:,:,:])
-    embedding = model.encoder(x)
-    e1 = embedding
-    recon = model.decoder(e1)
-    # print(recon.shape)
-    # print(recon)
-    for i in range(10):
-        recon_cpu = recon[i,:,:,:]#.detach().numpy()
-        recon_cpu = recon_cpu.cpu()
-        print(test_data_tensor[i,:,:,:].shape)
-        print(recon_cpu.shape)
-        plotimg(test_data_tensor[i,:,:,:], recon_cpu)
+    # x = model(test_data_tensor[0:64,:,:,:])
+    # embedding = model.encoder(x)
+    # e1 = embedding
+    # recon = model.decoder(e1)
+    # # print(recon.shape)
+    # # print(recon)
+    # for i in range(10):
+    #     recon_cpu = recon[i,:,:,:]#.detach().numpy()
+    #     recon_cpu = recon_cpu.cpu()
+    #     print(test_data_tensor[i,:,:,:].shape)
+    #     print(recon_cpu.shape)
+    #     plotimg(test_data_tensor[i,:,:,:], recon_cpu)
         
 
     return average_loss
