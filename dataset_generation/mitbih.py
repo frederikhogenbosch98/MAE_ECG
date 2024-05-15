17b97d5d5575ff4034666001de1416fbecc60335
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-05-15 17:31:45 +0200

    rerun

--- dataset_generation/mitbih.py
+++ dataset_generation/mitbih.py
@@ -72,7 +72,10 @@ def create_img_from_sign(lblabels, lbrevert_labels, lboriginal_labels, size=(224
         sig, _ = wfdb.rdsamp(_directory + file)
         ann = wfdb.rdann(_directory + file, extension='atr')
         len_sample = len(ann.sample)
-        for i in tqdm.tqdm(range(2, len_sample - 1)):
+        for i in tqdm.tqdm(range(2, len_sample - 2)):
+            print(range(2, len_sample - 2))
+            print(np.arange(1, len_sample-1))
+
             if ann.symbol[i] not in lboriginal_labels:
                 continue
             label = lboriginal_labels[ann.symbol[i]]
@@ -93,7 +96,7 @@ def create_img_from_sign(lblabels, lbrevert_labels, lboriginal_labels, size=(224
 
 
             rr_intervals = []
-            for j in nearest_integers(np.arange(1, len_sample), i):
+            for j in nearest_integers(np.arange(1, len_sample-1), i):
                 rr_intervals.append((ann.sample[j+1] - ann.sample[j])/360)
 
             mean_RR = np.mean(rr_intervals)
