daf601b21136b880fb41cef0b75813a44fdaf609
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-05-15 17:41:45 +0200

    rerun

--- dataset_generation/mitbih.py
+++ dataset_generation/mitbih.py
@@ -95,6 +95,8 @@ def create_img_from_sign(lblabels, lbrevert_labels, lboriginal_labels, size=(224
 
             rr_intervals = []
             for j in nearest_integers(np.arange(1, len_sample), i):
+                print(j+1)
+                print(nearest_integers(np.arange(1, len_sample)))[:-3]
                 rr_intervals.append((ann.sample[j+1] - ann.sample[j])/360)
 
             mean_RR = np.mean(rr_intervals)
