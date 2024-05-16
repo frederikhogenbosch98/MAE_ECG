efb97429b378336ad80389bf1c805b1742ce52e5
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-05-16 09:20:48 +0200

    selected files for mitbih

--- dataset_generation/mitbih.py
+++ dataset_generation/mitbih.py
@@ -54,11 +54,15 @@ def create_img_from_sign(lblabels, lbrevert_labels, lboriginal_labels, size=(224
 
     files = [f[:-4] for f in listdir(_directory) if isfile(join(_directory, f)) if (f.find('.dat') != -1)]
 
-    random.shuffle(files)
-    ds1 = files[: int(len(files) * _split_percentage)]
-    ds2 = files[int(len(files) * _split_percentage):]
-    ds11 = ds1[int(len(ds1) * _split_validation_percentage):]
-    ds12 = ds1[: int(len(ds1) * _split_validation_percentage)]
+    # random.shuffle(files)
+    # ds1 = files[: int(len(files) * _split_percentage)]
+    # ds2 = files[int(len(files) * _split_percentage):]
+    # ds11 = ds1[int(len(ds1) * _split_validation_percentage):]
+    # ds12 = ds1[: int(len(ds1) * _split_validation_percentage)]
+    ds11 = ['101', '106', '108', '109', '114', '115', '116', '119', '122', '209', '223']
+    ds12 = ['112', '118', '124', '201', '203', '205', '207', '208', '215', '220', '230']
+    ds2 = ['100', '103', '105', '111', '113', '117', '121', '123', '200', '202', '210', '212', '213',\
+           '214', '219', '221', '222', '228', '231', '232', '233', '234']
     print(f'ds11 files: {ds11}')
     print(f'ds12 files: {ds12}')
     print(f'ds2 files: {ds2}')
