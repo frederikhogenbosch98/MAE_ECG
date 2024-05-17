wrong dims

models/model_self_TD.py
@@ -115,7 +115,7 @@ class Classifier_self_TD(nn.Module):
         self.classifier = nn.Sequential(
                 # nn.Linear(50176+1, 256),
                 # nn.Linear(6272, 256), #16
-                nn.Linear(12544, 256), #32
+                nn.Linear(25088, 256), #32
                 nn.GELU(),
                 nn.BatchNorm1d(num_features=256),
                 nn.Dropout(0.5),