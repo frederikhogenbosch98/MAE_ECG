92dc3f218129b7f658c9b6e87e8f86f1fffbac77
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-05-19 13:23:26 +0200

    saveimg

--- reconstruct_check.py
+++ reconstruct_check.py
@@ -64,7 +64,7 @@ def eval_mae(model, testset, batch_size=128):
 
 
 if __name__ == "__main__":
-    device = torch.device("mps")
+    device = torch.device("cuda")
     transform = transforms.Compose([
         transforms.Grayscale(num_output_channels=1),
         transforms.Resize((112,112)), 
@@ -79,7 +79,7 @@ if __name__ == "__main__":
     # model = nn.DataParallel(AutoEncoder56())
     # model = AutoEncoder56_TD(R=20, in_channels=1, channels=[16, 32, 64]).to(device) 
     # model.load_state_dict(torch.load('trained_models/RUN_8_5_13_42/MAE_RUN_default_R0_8_5_13_42_epoch_40.pth', map_location=torch.device('cpu')))
-    model.load_state_dict(torch.load('trained_models/RUN_18_5_11_2/MAE_RUN_default_R0_18_5_11_4_epoch_179.pth', map_location=torch.device('cpu')))
+    model.load_state_dict(torch.load('trained_models/RUN_18_5_11_2/MAE_RUN_default_R0_18_5_11_4_epoch_179.pth')
     # model.load_state_dict(torch.load('trained_models/250_epoch_01_05_11am.pth', map_location=torch.device('cpu')))
     # model.load_state_dict(torch.load('trained_models/250_epoch_01_05_11am.pth', map_location=torch.device('cpu')))
 
