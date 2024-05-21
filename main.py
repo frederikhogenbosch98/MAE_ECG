0e00569897a7563765a2beed0e2f4439eb469ffb
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-05-21 08:43:18 +0200

    single gpu

--- main.py
+++ main.py
@@ -569,7 +569,7 @@ if __name__ == "__main__":
             else:
                 if args.gpu == 'all':
                     # mae = nn.DataParallel(AutoEncoder56_TD(R=R, in_channels=1, factorization=fact)).to(device)
-                    mae = nn.DataParallel(AutoEncoder11(R=R, in_channels=1)).to(device)
+                    mae = AutoEncoder11(R=R, in_channels=1).to(device)
                 else:
                     mae = AutoEncoder56_TD(R, in_channels=1, channels=channels).to(device) 
 
