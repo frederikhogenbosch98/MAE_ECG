242be0d8e1253024e0b1af0a0f29ca08672b5aab
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-05-23 14:38:14 +0200

    check resnet

--- models/resnet50.py
+++ models/resnet50.py
@@ -28,20 +28,30 @@ class ResidualBlock(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, layers, num_classes = 7):
+    def __init__(self, layers=[1, 1, 3, 1], num_classes = 7):
         super(ResNet, self).__init__()
-        self.inplanes = 64
+        self.inplanes = 32
+        self.outplanes = 256
         self.conv1 = nn.Sequential(
                         nn.Conv2d(1, 64, kernel_size = 7, stride = 2, padding = 3),
                         nn.BatchNorm2d(64),
                         nn.ReLU())
         self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
-        self.layer0 = self._make_layer(ResidualBlock, 64, layers[0], stride = 1)
-        self.layer1 = self._make_layer(ResidualBlock, 128, layers[1], stride = 2)
-        self.layer2 = self._make_layer(ResidualBlock, 256, layers[2], stride = 2)
-        self.layer3 = self._make_layer(ResidualBlock, 512, layers[3], stride = 2)
-        self.avgpool = nn.AvgPool2d(4, stride=1)
-        self.fc = nn.Linear(512, num_classes)
+        self.downlayer0 = self._make_layer(ResidualBlock, 32, layers[0], stride = 1)
+        self.downlayer1 = self._make_layer(ResidualBlock, 64, layers[1], stride = 2)
+        self.downlayer2 = self._make_layer(ResidualBlock, 128, layers[2], stride = 2)
+        self.downlayer3 = self._make_layer(ResidualBlock, 256, layers[3], stride = 2)
+
+        self.uplayer1 = self._make_uplayer(ResidualBlock, 256, layers[3], stride = 2) 
+        self.uplayer2 = self._make_uplayer(ResidualBlock, 128, layers[2], stride = 2)
+        self.uplayer3 = self._make_uplayer(ResidualBlock, 64, layers[1], stride = 2)
+        self.uplayer4 = self._make_uplayer(ResidualBlock, 32, layers[0], stride = 1) 
+
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear')
+        self.upconv1 =  nn.Sequential(
+                        nn.ConvTranspose2d(64, 1, kernel_size = 7, stride = 2, padding = 3),
+                        nn.BatchNorm2d(64),
+                        nn.ReLU()) 
         
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
@@ -58,18 +68,37 @@ class ResNet(nn.Module):
             layers.append(block(self.inplanes, planes))
 
         return nn.Sequential(*layers)
+
+
+    def _make_uplayer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.outplanes != planes:
+            
+            downsample = nn.Sequential(
+                nn.ConvTranspose2d(self.outplanes, planes, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(planes),
+            )
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.outplanes = planes
+        for i in range(1, blocks):
+            layers.append(block(self.outplanes, planes))
+
+        return nn.Sequential(*layers) 
     
     
     def forward(self, x):
         x = self.conv1(x)
         x = self.maxpool(x)
-        x = self.layer0(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
+        x = self.downlayer0(x)
+        x = self.downlayer1(x)
+        x = self.downlayer2(x)
+        x = self.downlayer3(x)
+        x = self.uplayer1(x)
+        x = self.uplayer2(x)
+        x = self.uplayer3(x)
+        x = self.uplayer4(x)
+        x = self.upsample(x)
+        x = self.upconv1(x)
 
         return x
