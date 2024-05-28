b32141af8f017c4213a399ea74dd5d5ec952808d
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-05-28 08:17:22 +0200

    model select

--- model_selection.py
+++ model_selection.py
@@ -149,7 +149,7 @@ if __name__ == "__main__":
     class_losses_run = np.zeros((4, num_epochs_classifier))
     class_val_losses_run = np.zeros((4, num_epochs_classifier))
 
-    models = [UNet(), AutoEncoder11_UN(), ResNet(), ConvNext()] # ConvNext(),
+    models = [ AutoEncoder11_UN(),UNet(), ResNet(), ConvNext()] # ConvNext(),
     model_strs = ['unet_32', 'basic', 'resnet', 'convnext'] # 'convnext', 
     lr = [5e-5, 1e-4, 1e-4, 1e-4]
 
