95861563104accf8a138d86bead4200d982a06a8
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-05-28 08:16:39 +0200

    extra deep un32

--- models/_11am_un.py
+++ models/_11am_un.py
@@ -38,29 +38,29 @@ class AutoEncoder11_UN(nn.Module):
             nn.BatchNorm2d(channels[2]),
             nn.GELU(),
             # LAYER 6
+            nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(channels[2], channels[3], kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(channels[3]),
+            nn.GELU(),
+            # LAYER 5
+            nn.Conv2d(channels[3], channels[3], kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(channels[3]),
+            nn.GELU(),
+            # LAYER 6
             nn.MaxPool2d(2, stride=2)
-            # nn.Conv2d(channels[2], channels[3], kernel_size=3, stride=1, padding=1),
-            # nn.BatchNorm2d(channels[3]),
-            # nn.GELU(),
-            # # LAYER 5
-            # nn.Conv2d(channels[3], channels[3], kernel_size=3, stride=1, padding=1),
-            # nn.BatchNorm2d(channels[3]),
-            # nn.GELU(),
-            # # LAYER 6
-            # nn.MaxPool2d(2, stride=2)
             
         )
 
         self.decoder = nn.Sequential(
             # Corresponds to LAYER 6 in Encoder
-            # nn.Upsample(scale_factor=2, mode='nearest'),
-            # nn.Conv2d(channels[3], channels[3], kernel_size=3, stride=1, padding=1),
-            # nn.BatchNorm2d(channels[3]),
-            # nn.GELU(),
-            # # Corresponds to LAYER 5 in Encoder
-            # nn.Conv2d(channels[3], channels[2], kernel_size=3, stride=1, padding=1),
-            # nn.BatchNorm2d(channels[2]),
-            # nn.GELU(),
+            nn.Upsample(scale_factor=2, mode='nearest'),
+            nn.Conv2d(channels[3], channels[3], kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(channels[3]),
+            nn.GELU(),
+            # Corresponds to LAYER 5 in Encoder
+            nn.Conv2d(channels[3], channels[2], kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(channels[2]),
+            nn.GELU(),
             nn.Upsample(scale_factor=2, mode='bilinear'),
             nn.Conv2d(channels[2], channels[2], kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(channels[2]),
@@ -95,7 +95,7 @@ class AutoEncoder11_UN(nn.Module):
 
     def forward(self, x):
         x = self.encoder(x)
-        # print(x.shape)
+        print(x.shape)
         x = self.decoder(x)
         return x
 
