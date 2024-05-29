097f8df2dd5d8b41aa4d27b45c6b8d3b3d0e9f59
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-05-29 09:55:28 +0200

    1028

--- models/_11am_un.py
+++ models/_11am_un.py
@@ -108,10 +108,10 @@ class Classifier_UN(nn.Module):
                 # nn.Linear(50176+1, 256),
                 # nn.Linear(6272, 256), #16
                 # nn.Linear(32768+1, 256), #32
-                nn.Linear(16384+1, 512), #32
+                nn.Linear(16384+1, 1028), #32
                 # nn.Linear(16*16*256, 256), #32
                 nn.GELU(),
-                nn.BatchNorm1d(num_features=512),
+                nn.BatchNorm1d(num_features=1028),
                 nn.Dropout(0.5),
                 nn.Linear(512, out_features)
         )
