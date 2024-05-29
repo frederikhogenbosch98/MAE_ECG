aaff2785fe2bbc6a13c0f76d8a4227b0ecc4f4d8
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-05-29 08:45:05 +0200

    unet

--- main.py
+++ main.py
@@ -211,7 +211,8 @@ def train_mae(model, trainset, run_dir, device, min_lr=1e-5, valset=None, weight
         # model.load_state_dict(torch.load('data/models_mnist/MAE_TESTRUN.pth'))
         # model.load_state_dict(torch.load('trained_models/MAE_RUN_cp_R0_8_5_4_38.pth', map_location=torch.device('cpu')))
         # model.load_state_dict(torch.load('trained_models/last/last_run.pth'))
-        model.load_state_dict(torch.load('trained_models/model_comparison/RUN_26_5_23_0_exprun/MAE_RUN_convnext_R0_27_5_17_21_epoch_20.pth')) #unet
+        # model.load_state_dict(torch.load('trained_models/model_comparison/RUN_26_5_23_0_exprun/MAE_RUN_convnext_R0_27_5_17_21_epoch_20.pth')) #unet
+        model.load_state_dict(torch.load('trained_models/model_comparison/RUN_26_5_23_0_exprun/MAE_RUN_unet_32_R0_26_5_23_0.pth')) #convnext
         # model.load_state_dict(torch.load('trained_models/RUN_19_5_23_14/MAE_RUN_cp_R25_20_5_11_41.pth')) #R25
         # model.load_state_dict(torch.load('trained_models/RUN_14_5_22_16/MAE_RUN_default_R0_14_5_22_16.pth'))
         # model.load_state_dict(torch.load('trained_models/tranpose_02_05_10am.pth', map_location=torch.device('cpu')))
