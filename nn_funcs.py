f2964bf5e25f1c9aa81dcd658da53bfdac22bfb7
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-05-29 17:46:22 +0200

    bin variable

--- nn_funcs.py
+++ nn_funcs.py
@@ -212,12 +212,16 @@ class MITBIHImageWithFeatureDataset(torch.utils.data.Dataset):
         # Load feature
         with open(feature_path, 'r') as f:
             feature = float(f.read().strip())
+            if feature > 0.04:
+                feature = 1
+            else:
+                feature = 0
 
         # Scale feature
-        if self.scale_method == 'normalize':
-            feature = (feature - self.feature_min) / (self.feature_max - self.feature_min)
-        elif self.scale_method == 'standardize':
-            feature = (feature - self.feature_mean) / self.feature_std
+        # if self.scale_method == 'normalize':
+        #     feature = (feature - self.feature_min) / (self.feature_max - self.feature_min)
+        # elif self.scale_method == 'standardize':
+        #     feature = (feature - self.feature_mean) / self.feature_std
 
         # Convert feature to tensor
         feature = torch.tensor([feature], dtype=torch.float32)
@@ -268,9 +272,9 @@ class INCARTDBImageWithFeatureDataset(torch.utils.data.Dataset):
 
         # Convert features to tensor
         # print(len(self.features))
-        self.features = torch.tensor(self.features, dtype=torch.float16)
+        self.features = torch.tensor(self.features, dtype=torch.float32)
         # print((self.features > 0.04).sum().item())
-        self.features = torch.where(self.features > 0.04, torch.tensor(1), torch.tensor(0))
+        # self.features = torch.where(self.features > 0.04, torch.tensor(1), torch.tensor(0))
         # print(self.features.shape)
         # print(len(self.image_paths))
         
@@ -306,6 +310,10 @@ class INCARTDBImageWithFeatureDataset(torch.utils.data.Dataset):
         # Load feature
         with open(feature_path, 'r') as f:
             feature = float(f.read().strip())
+            if feature > 0.04:
+                feature = 1
+            else:
+                feature = 0
 
         # Scale feature
         # if self.scale_method == 'normalize':
