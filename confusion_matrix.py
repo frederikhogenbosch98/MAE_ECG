b1f347e9e0c2cf77222aff5831854ed0954593a2
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-06-09 15:43:02 +0200

    will this work

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -2,7 +2,7 @@ from nn_funcs import conf_matrix, MITBIHImageWithFeatureDataset
 from torchvision import transforms
 import torch
 import numpy as np
-from models._11am_un import AutoEncoder11_UN
+from models._11am_un import AutoEncoder11_UN, Classifier_UN
 from models._11am_back import AutoEncoder11
 
 if __name__ == "__main__":
@@ -20,12 +20,14 @@ if __name__ == "__main__":
     mitbih_dataset_test = MITBIHImageWithFeatureDataset(root_dir=mitbih_ds2_dir, transform=transform) 
     testset_sup = mitbih_dataset_test
 
-    model = AutoEncoder11(R=100, factorization='cp')
+    # mae = AutoEncoder11(R=100, factorization='cp')
+    mae = AutoEncoder11_UN()
+    classifier = Classifier_UN(mae=mae, out_features=5)
 
-    model.load_state_dict(torch.load('trained_models/model_comparison/RUN_30_5_11_11_uncompressed_baseline/CLASSIFIER_RUN_basic_R0_30_5_11_11.pth')) 
+    classifier.load_state_dict(torch.load('trained_models/model_comparison/RUN_30_5_11_11_uncompressed_baseline/CLASSIFIER_RUN_basic_R0_30_5_11_11.pth')) 
 
-    model.to(device)
-    model.eval()
+    classifier.to(device)
+    classifier.eval()
 
     test_loader = torch.utils.data.DataLoader(testset_sup, 
                                             batch_size=128, 
@@ -38,7 +40,7 @@ if __name__ == "__main__":
     with torch.no_grad():
         for images, features, labels in test_loader:
             images, features, labels = images.to(device), features.to(device), labels.to(device)
-            output = model(images, features)
+            output = classifier(images, features)
 
             _, predicted = torch.max(output.data, 1)
             for i in range(len(labels)):
