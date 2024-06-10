75315e5d0f6fff9f48c908562a8654815c364780
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-06-10 10:38:53 +0200

    lower comsp again

--- compression_run.py
+++ compression_run.py
@@ -154,13 +154,13 @@ if __name__ == "__main__":
     num_params_uncompressed = 9411649#2355745
 
     
-    R_LIST = [25, 35, 50, 75, 100, 125, 150, 200] # 5, 10, 15, 20
+    R_LIST = [125, 150, 200] # 5, 10, 15, 20, 25, 35, 50, 75, 100,
 
     CLASSIFY = True
     fact = 'cp'
 
     now = datetime.now()
-    run_dir = f'trained_models/compressed/RUN_{now.day}_{now.month}_{now.hour}_{now.minute}_full_comp_run_rerun_lowerlr'
+    run_dir = f'trained_models/compressed/RUN_{now.day}_{now.month}_{now.hour}_{now.minute}_full_comp_run_rerun_lowerlr2'
     os.makedirs(f'{run_dir}/', exist_ok=True)
     for i, R in enumerate(R_LIST):
         print(f'RUN R: {R}')
