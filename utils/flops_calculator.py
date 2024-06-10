27fa1995502f4f8223da4cbe3170da4ab19ff973
frederikhogenbosch98 <fhogenbosch@icloud.com>
2024-06-10 09:53:30 +0200

    lets see

--- utils/flops_calculator.py
+++ utils/flops_calculator.py
@@ -12,11 +12,13 @@ from resnet50 import ResNet
 from UNet import UNet
 from convnext import ConvNext
 from _11am_un import AutoEncoder11_UN
+from _11am_back import AutoEncoder11
 
 x = torch.randn(1, 1, 128, 128)
 
+for r in [5, 10, 15, 20, 25, 35, 50, 75, 100, 125, 150, 175, 200]:
 
-flops = FlopCountAnalysis(ConvNext(), x)
+    flops = FlopCountAnalysis(AutoEncoder11(R=r), x)
 
-print(f"FLOPs: {flops.total()}")
-print(flops.by_module())
+    print(f"FLOPs: {flops.total()}, R: {r}")
+    # print(flops.by_module())
